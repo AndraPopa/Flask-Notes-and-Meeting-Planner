cbb7fb944195bf9b7522aeb99a676f1d131e6634
AndraPopa <93611644+AndraPopa@users.noreply.github.com>
2023-10-26 16:34:33 +0300

    added the navbar, the base template with some css stuff imported (bootstrap)

--- planner/views.py
+++ planner/views.py
@@ -1,8 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
 
 @views.route('/')
 def index():
-    return "<h1>Home page</h1>"
+    return render_template('home.html')
