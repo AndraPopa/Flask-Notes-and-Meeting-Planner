0a07a5f6d542f47c599899db3d2ee9cb28f28083
AndraPopa <93611644+AndraPopa@users.noreply.github.com>
2023-10-27 17:09:38 +0300

    login and logout user

--- planner/__init__.py
+++ planner/__init__.py
@@ -1,6 +1,7 @@
 from os import path
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = "planner_database.db"
@@ -25,5 +26,13 @@ def create_app():
         if not path.exists(f'instance/{DB_NAME}'):
             db.create_all()
             print('Created Database!')
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
     return app
--- planner/auth.py
+++ planner/auth.py
@@ -28,7 +28,7 @@ def login():
 
 
 @auth.route('/logout')
-@login.required
+@login_required
 def logout():
     logout_user()
     return redirect(url_for('auth.login'))
